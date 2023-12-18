e023ab48a2fcc9b3c03a1a1ccc9758310337e088
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2023-12-19 00:54:42 +0600

    Enabled PWA

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,5 @@
 import tailwind from "@astrojs/tailwind";
+import AstroPWA from "@vite-pwa/astro";
 import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
@@ -18,6 +19,79 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     sitemap(),
+    AstroPWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        background_color: "#ffffff",
+        description: "Express your creativity!",
+        dir: "ltr",
+        display: "standalone",
+        name: "The Writers Workshop Bangladesh",
+        orientation: "any",
+        scope: "/",
+        short_name: "The Writers Workshop Bangladesh",
+        start_url: "/",
+        theme_color: "#7b00e6",
+        categories: [
+          "entertainment",
+          "kids",
+          "lifestyle",
+          "magazines",
+          "social",
+        ],
+        icons: [
+          {
+            src: "pwa-192x192.webp",
+            sizes: "192x192",
+          },
+          {
+            src: "pwa-512x512.webp",
+            sizes: "512x512",
+          },
+          {
+            src: "pwa-512x512-square.webp",
+            sizes: "512x512",
+            purpose: "any",
+          },
+        ],
+        screenshots: [
+          {
+            src: "mobile-screenshot.webp",
+            sizes: "1082x2402",
+            form_factor: "narrow",
+          },
+          {
+            src: "desktop-screenshot.webp",
+            sizes: "1744x857",
+            form_factor: "wide",
+          },
+        ],
+        shortcuts: [
+          {
+            name: "Stories",
+            url: "/stories",
+            description: "Stories",
+          },
+          {
+            name: "Poems",
+            url: "/poems",
+            description: "Poems",
+          },
+          {
+            name: "Comics",
+            url: "/comics",
+            description: "Comics",
+          },
+        ],
+        id: "thewritersworkshopbd",
+        edge_side_panel: {
+          preferred_width: 400,
+        },
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+    }),
   ],
   vite: {
     css: {
